b3f09ab27f096000c242c6a29a942dbcf667a737
admin <admin@Akash-MacBook-Pro.local>
2022-09-13 11:31:02 +0530

    enabled video call

--- App.tsx
+++ App.tsx
@@ -1,120 +1,146 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
-
-import React, {type PropsWithChildren} from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import {
-  SafeAreaView,
+  Platform,
   ScrollView,
-  StatusBar,
-  StyleSheet,
   Text,
-  useColorScheme,
+  TouchableOpacity,
   View,
 } from 'react-native';
+import RtcEngine, {
+  RtcLocalView,
+  RtcRemoteView,
+  VideoRenderMode,
+} from 'react-native-agora';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import requestCameraAndAudioPermission from './components/Permission';
+import styles from './components/Style';
 
-const Section: React.FC<
-  PropsWithChildren<{
-    title: string;
-  }>
-> = ({children, title}) => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
+const config = {
+  appId: "8c7c96fa8c0546db919c842a796cff88",
+  token: "007eJxTYHjiolahkxn9/Gegk0fbnUMznuqKnpl788MDhWcbHVa8zjyqwGCRbJ5saZaWaJFsYGpilpJkaWiZbGFilGhuaZaclmZhkc+jkPw+VjH5VMphBiYGRjAE8VkYSlKLSxgYWBlAAAD0kyOh",
+  channelName: 'test',
 };
 
 const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
+  const _engine = useRef<RtcEngine | null>(null);
+  const [isJoined, setJoined] = useState(false);
+  const [peerIds, setPeerIds] = useState<number[]>([]);
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  useEffect(() => {
+    if(Platform.OS == 'android'){
+      requestCameraAndAudioPermission().then(() => {
+        console.log('requested!');
+      });
+    }
+  }, []);
+
+  useEffect(() => {
+    const init = async () => {
+      const { appId } = config;
+      _engine.current = await RtcEngine.create(appId);
+      await _engine.current.enableAudio();
+      await _engine.current.enableVideo();
+
+      _engine.current.addListener('Warning', (warn) => {
+        console.log('Warning', warn);
+      });
+
+      _engine.current.addListener('Error', (err) => {
+        console.log('Error', err);
+      });
+
+      _engine.current.addListener('UserJoined', (uid, elapsed) => {
+        console.log('UserJoined', uid, elapsed);
+        if (peerIds.indexOf(uid) === -1) {
+          setPeerIds((prev) => [...prev, uid]);
+        }
+      });
+
+      _engine.current.addListener('UserOffline', (uid, reason) => {
+        console.log('UserOffline', uid, reason);
+        setPeerIds((prev) => prev.filter((id) => id !== uid));
+      });
+
+      _engine.current.addListener(
+        'JoinChannelSuccess',
+        (channel, uid, elapsed) => {
+          console.log('JoinChannelSuccess', channel, uid, elapsed);
+          setJoined(true);
+        }
+      );
+    };
+    init();
+  }, []);
+
+  const startCall = async () => {
+    await _engine.current?.joinChannel(
+      config.token,
+      config.channelName,
+      null,
+      0
+    );
+  };
+
+  const endCall = async () => {
+    await _engine.current?.leaveChannel();
+    setPeerIds([]);
+    setJoined(false);
+  };
+
+  const _renderVideos = () => {
+    return isJoined ? (
+      <View style={styles.fullView}>
+        <RtcLocalView.SurfaceView
+          style={styles.max}
+          channelId={config.channelName}
+          renderMode={VideoRenderMode.FILL}
+        />
+        {_renderRemoteVideos()}
+      </View>
+    ) : null;
+  };
+
+  const _renderRemoteVideos = () => {
+    return (
+      <ScrollView
+        style={styles.remoteContainer}
+        contentContainerStyle={styles.padding}
+        horizontal={true}
+      >
+        {peerIds.map((value,index) => {
+          return (
+            <View style={{borderWidth: 2,}} key={index}>
+
+            <RtcRemoteView.SurfaceView
+            key={index.toString()}
+            style={styles.remote}
+            uid={value}
+            channelId={config.channelName}
+            renderMode={VideoRenderMode.Hidden}
+            zOrderMediaOverlay={true}
+            />
+            </View>
+          );
+        })}
+      </ScrollView>
+    );
   };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+    <View style={styles.max}>
+      <View style={styles.max}>
+        <View style={styles.buttonHolder}>
+          <TouchableOpacity onPress={startCall} style={styles.button}>
+            <Text style={styles.buttonText}> Start Call </Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={endCall} style={styles.button}>
+            <Text style={styles.buttonText}> End Call </Text>
+          </TouchableOpacity>
         </View>
-      </ScrollView>
-    </SafeAreaView>
+        {_renderVideos()}
+      </View>
+    </View>
   );
 };
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
